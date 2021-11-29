a4a9dd3db2dc784d02d89ac92c75a79207ef8952
Tristan Elliott <>
2021-11-29 17:38:21 -0400

    initial commit

---
+++ src/main/java/com/ECalvingWeb/ECalvingWeb/ECalvingWebApplication.java
@@ -0,0 +1,13 @@
+package com.ECalvingWeb.ECalvingWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECalvingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECalvingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ECalvingWeb/ECalvingWeb/ECalvingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECalvingWeb.ECalvingWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECalvingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
